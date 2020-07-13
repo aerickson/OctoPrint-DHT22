4ac147b80cca67d85350aac0b8d2083020850d6e
Andrew Erickson <aerickson@gmail.com>
2020-07-12 23:24:48 -0700

    add description

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.4"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = """Reads DHT22 sensors and adds the data to OctoPrint's temperature data."""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Andrew J. Erickson"
